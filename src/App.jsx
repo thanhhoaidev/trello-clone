6c27b2bd1d3ac725d5f0088cbe185738ea31ef9b
nth090801 <thanhhoai160518@gmail.com>
2024-03-18 22:19:25 +0700

    Dark/Light Mode #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
-    
+      <ModeToggle></ModeToggle>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
